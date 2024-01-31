6dd1f2484171df6939f718dc7f25abd93224614e
geooclarke <131209920+geooclarke@users.noreply.github.com>
2024-01-31 15:38:53 +0000

    Update createCameraRig.py
    - Adjusted location of defining functions
    - Removed an error of printing old variable that no longer exists

--- createCameraRig.py
+++ createCameraRig.py
@@ -1,18 +1,21 @@
+# creates a camera rig, locks the correct attributes and assigns a distance dimension node that is then plugged into the DOF as an expression
+
 import maya.cmds as cmds
 
 #define camera creation function
-def create_camera(name):
-    create_rendercam = cmds.camera(name=name)
+def create_camera():
+    create_rendercam = cmds.camera()
+    camera_list = create_rendercam
     camera_attrs = ['translate', 'rotate']
-    camera_axis = ['X', 'Y']a
+    camera_axis = ['X', 'Y']
     obj = cmds.ls(sl=True)[0]
     
     
     # nested for loop to run through each attribute and lock accordingly
     for axis in camera_axis:
         for attr in camera_attrs:
-            cmds.setAttr(obj+'.'+attr+axis, lock=1)
-    return name
+            cmds.setAttr(f"{obj}.{attr}{axis}", lock=1)
+    return camera_list
 
 # define the function that will lock the attributes on the tilt group
 def lock_tilt_attrs(name):
@@ -24,11 +27,11 @@ def lock_tilt_attrs(name):
         for axis in tilt_axis:
             if axis != "X":
                 for attr in tilt_attrs:
-                    cmds.setAttr(obj+"."+attr+axis, lock=1)
+                    cmds.setAttr(f"{obj}.{attr}{axis}", lock=1)
             else:
                 for attr in tilt_attrs:
                     if attr != "rotate":
-                        cmds.setAttr(obj+"."+attr+axis, lock=1)
+                        cmds.setAttr(f"{obj}.{attr}{axis}", lock=1)
         return
 
 # define the function that will lock the the attributes on the crane group
@@ -39,31 +42,31 @@ def lock_crane_attrs(name):
         
         for axis in tilt_axis:
             for attr in tilt_attrs:
-                cmds.setAttr(obj+"."+attr+axis, lock=1)
+                cmds.setAttr(f"{obj}.{attr}{axis}", lock=1)
         return
 
-#define the group creation
+# define the group creation
 def create_grp(name):
     cmds.group(name=name) # indexing is so that it only selects the transform node
     return name
 
-# creation of camera and groups, as well as locking values
-cam = create_camera("camera_1")
-cam = cmds.ls(sl=True)[0]
-tilt_grp = create_grp(f"{cam}_tilt")
-lock_tilt_attrs((cmds.ls(sl=True)[0]))
-crane_grp = create_grp(f"{cam}_crane")
-lock_crane_attrs((cmds.ls(sl=True)[0]))
-
-# new creation of the locators and distance object
-
+# define locator
 def createLocator(name):
     name = name
     cmds.spaceLocator(n=name)
     return name
 
-loc_1 = createLocator(f"{cam}_DoF_loc_1")
-loc_2 = createLocator(f"{cam}_DoF_loc_2")
+# creation of camera and groups, as well as locking values
+cam = create_camera()
+camMain = cam[0]
+tilt_grp = create_grp(f"{camMain}_tilt")
+lock_tilt_attrs((cmds.ls(sl=True)[0]))
+crane_grp = create_grp(f"{camMain}_crane")
+lock_crane_attrs((cmds.ls(sl=True)[0]))
+
+# creation of the locators and distance object
+loc_1 = createLocator(f"{camMain}_DoF_loc_1")
+loc_2 = createLocator(f"{camMain}_DoF_loc_2")
 
 cmds.move(0, 1, 0, loc_1)
 startPoint = cmds.getAttr(f"{loc_1}.translate")
@@ -73,10 +76,10 @@ create_dist = cmds.distanceDimension(sp=startPoint[0], ep=endPoint[0])
 cmds.move(0, 0, 0, loc_1)
 distDimParent = cmds.listRelatives(create_dist, p=True)
 cmds.select(clear=True)
-distDimParent = cmds.rename(distDimParent, f"{cam}_DoF")
+distDimParent = cmds.rename(distDimParent, f"{camMain}_DoF")
 # parenting to the main group
-cmds.parent(loc_2, cam)
+cmds.parent(loc_2, camMain)
 cmds.parent(loc_1, crane_grp)
 cmds.parent(distDimParent, crane_grp)
 
-cmds.expression(o = cam, s = (cam+".focusDistance = "+distDimParent+".distance"))
+cmds.expression(o=camMain, s=(f"{camMain}.focusDistance = {distDimParent}.distance"))
