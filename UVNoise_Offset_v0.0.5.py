8c88effc8bd0f164dfc2dbbabcb707f050e462e1
geooclarke <131209920+geooclarke@users.noreply.github.com>
2024-10-28 16:40:42 +0000

    Add files via upload

---
+++ UVNoise_Offset_v0.0.5.py
@@ -0,0 +1,17 @@
+# select the place 2d nodes that you want to plug the maxon noise node into 
+# then run script
+
+import maya.cmds as cmds
+
+selected = cmds.ls(sl=True)
+
+maxon_noise = cmds.shadingNode("RedshiftMaxonNoise", asTexture=True)
+remap_node = cmds.shadingNode("remapValue", asUtility=True)
+cmds.connectAttr(f"{maxon_noise}.outColorR", f"{remap_node}.inputValue")
+cmds.setAttr(f"{maxon_noise}.noise_type", 24)
+cmds.setAttr(F"{remap_node}.outputMax", 10)
+
+for node in selected:
+    if cmds.objectType(node, isType="place2dTexture"):
+        cmds.connectAttr(f"{remap_node}.outValue", f"{node}.offset.offsetU")
+        cmds.connectAttr(f"{remap_node}.outValue", f"{node}.offset.offsetV")
